c697498cfc9a6a1fbf31402225dd15aa9fef12c3
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-14 17:35:16 -0700

    jsString v2

--- js/custom-quiz.js
+++ js/custom-quiz.js
@@ -134,12 +134,12 @@ function mapLoaded() {
 function generateHighlightJS() {
     let jsString
     let code = ""
-    // for (let i = 0; i < fullCountryList.length; i++) {
-    //     jsString = '$("#thing-' + fullCountryList[i] + '").click(function(a){a.preventDefault();a=$("#thing-' + fullCountryList[i] + '").mouseout().data("maphilight")||{};a.alwaysOn=!a.alwaysOn,console.log("' + fullCountryList[i] + ' is now "} a.alwaysOn),$("#thing-${fullCountryList[i]}").data("maphilight",a).trigger("alwaysOn.maphilight")});'
-    //     console.log(jsString)
-    //     code += jsString
-    // }
-    // document.getElementById("country-scripts").innerHTML = code
+    for (let i = 0; i < fullCountryList.length; i++) {
+        jsString = '$("#thing-' + fullCountryList[i] + '").click(function(a){a.preventDefault();a=$("#thing-' + fullCountryList[i] + '").mouseout().data("maphilight")||{};a.alwaysOn=!a.alwaysOn,console.log("' + fullCountryList[i] + ' is now "} a.alwaysOn),$("#thing-${fullCountryList[i]}").data("maphilight",a).trigger("alwaysOn.maphilight")});'
+        console.log(jsString)
+        code += jsString
+    }
+    document.getElementById("country-scripts").innerHTML = code
 }
 
 function loadFromJSON(url) {
