aad5582e1ad1a9ccab912119844bf8e57cacd70f
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-14 17:17:35 -0700

    try without  ``

--- js/custom-quiz.js
+++ js/custom-quiz.js
@@ -136,7 +136,7 @@ function generateHighlightJS() {
     let jsString
     let code
     for (let i = 0; i < fullCountryList.length; i++) {
-        jsString = `$("#thing-${fullCountryList[i]}").click(function(a){a.preventDefault();a=$("#thing-${fullCountryList[i]}").mouseout().data("maphilight")||{};a.alwaysOn=!a.alwaysOn,console.log(${fullCountryList[i] + " is now "}a.alwaysOn),$("#thing-${fullCountryList[i]}").data("maphilight",a).trigger("alwaysOn.maphilight")});`
+        jsString = '$("#thing-' + fullCountryList[i] + '").click(function(a){a.preventDefault();a=$("#thing-' + fullCountryList[i] + '").mouseout().data("maphilight")||{};a.alwaysOn=!a.alwaysOn,console.log(' + fullCountryList[i] + '" is now "} a.alwaysOn),$("#thing-${fullCountryList[i]}").data("maphilight",a).trigger("alwaysOn.maphilight")});'
         code += jsString
     }
     document.getElementById("country-scripts").innerHTML = code
