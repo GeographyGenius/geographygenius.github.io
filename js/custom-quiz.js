c82e9ab257b0ddaa8b1f5ba250f870183bc67c4e
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-17 16:32:46 -0700

    parse int is not a function

--- js/custom-quiz.js
+++ js/custom-quiz.js
@@ -348,7 +348,7 @@ function setCountryVisible(countryNumber, value) {
     } else {
         bit = "0"
     }
-    console.log(value, bit, countryNumber.parseInt())
-    binaryExcludeData = setCharAt(binaryExcludeData, countryNumber.parseInt(), bit)
+    console.log(value, bit, parseInt(countryNumber))
+    binaryExcludeData = setCharAt(binaryExcludeData, parseInt(countryNumber), bit)
     console.log(binaryExcludeData)
 }
