e14a6b6b7a70883866e617826b9f3b1aa22f01c5
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-17 16:27:12 -0700

    um

--- js/custom-quiz.js
+++ js/custom-quiz.js
@@ -349,6 +349,6 @@ function setCountryVisible(countryNumber, value) {
         bit = "0"
     }
     console.log(value, bit, countryNumber)
-    binaryExcludeData = setCharAt(binaryExcludeData, countryNumber, bit)
+    binaryExcludeData = setCharAt(binaryExcludeData, bit, countryNumber)
     console.log(binaryExcludeData)
 }
