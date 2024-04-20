fd6e4c3a96bc1f2365869e4eba3643a1b0d86c27
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-20 14:24:35 -0700

    smaller min width

--- js/custom-quiz.js
+++ js/custom-quiz.js
@@ -114,7 +114,7 @@ function mapLoaded() {
     let checkboxesString = ""
     let i = 0
     while (i < fullCountryList.length) {
-        checkboxesString += '<td valign="top" style="font-size:small;" min-width="250px" width="250px">'
+        checkboxesString += '<td valign="top" style="font-size:small;" min-width="200px" width="230px">'
         for (let count = 0; count < 28 && i < fullCountryList.length; i++, count++) {
             checkboxesString += `<input type="checkbox" id="${spaceToHyphen(fullCountryList[i]) + "-checkbox"}" name="${spaceToHyphen(fullCountryList[i]) + "-checkbox"}" value="${spaceToHyphen(fullCountryList[i])}">\n<label for="${spaceToHyphen(fullCountryList[i]) + "-checkbox"}"> ${fullCountryList[i]}</label><br>\n`
         }
