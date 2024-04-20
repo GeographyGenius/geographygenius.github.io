136982522031e910aa352e9766cf622d0d9cfb30
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-20 09:28:03 -0700

    try again v4

--- js/custom-quiz.js
+++ js/custom-quiz.js
@@ -114,8 +114,8 @@ function mapLoaded() {
     let checkboxesString = ""
     let i = 0
     while (i < fullCountryList.length) {
-        checkboxesString += '<td valign="top" style="font-size:small;">'
-        for (let count = 0; count < 32 && i < fullCountryList.length; i++, count++) {
+        checkboxesString += '<td valign="top" min-width="250px">' //" style="font-size:small;
+        for (let count = 0; count < 28 && i < fullCountryList.length; i++, count++) {
             checkboxesString += `<input type="checkbox" id="${spaceToHyphen(fullCountryList[i]) + "-checkbox"}" name="${spaceToHyphen(fullCountryList[i]) + "-checkbox"}" value="${spaceToHyphen(fullCountryList[i])}">\n<label for="${spaceToHyphen(fullCountryList[i]) + "-checkbox"}"> ${fullCountryList[i]}</label><br>\n`
         }
         checkboxesString += "</td>"
