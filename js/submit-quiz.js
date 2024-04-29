2e3ef89c7e419150185b2977f041d62fa8c13411
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-28 18:00:00 -0700

    data param, not url

--- js/submit-quiz.js
+++ js/submit-quiz.js
@@ -2,10 +2,10 @@ let searchParams = new URLSearchParams(window.location.search)
 
 setup()
 function setup() {
-    if (!(searchParams.has("url"))) {
+    if (!(searchParams.has("data"))) {
         window.location.href = "/"
     } else {
-        let quizURL = "https://" + window.location.host + "/geography/quiz" + atob(searchParams.get("url"))
+        let quizURL = "https://" + window.location.host + "/geography/quiz" + atob(searchParams.get("data"))
         formDiv = document.getElementById("form-container")
         formDiv.innerHTML = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmVw7wO8HzBLRiUNQOuxEbDJIh0vMw7RqNr7w7-67pgmDI0w/viewform?embedded=true&entry.1624436263=${encodeURIComponent(quizURL)}" width="640" height="448" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`
     }
