769cd53991de5489a9a5c8a3ac1ab7d10035b3f1
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-28 18:38:02 -0700

    done with form

--- js/submit-quiz.js
+++ js/submit-quiz.js
@@ -10,6 +10,6 @@ function setup() {
         let quizURL = "https://" + window.location.host + "/geography/quiz" + atob(searchParams.get("data"))
         console.log(quizURL)
         formDiv = document.getElementById("form-container")
-        formDiv.innerHTML = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmVw7wO8HzBLRiUNQOuxEbDJIh0vMw7RqNr7w7-67pgmDI0w/viewform?entry.1624436263=${encodeURIComponent(quizURL)}" width="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`
+        formDiv.innerHTML = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmVw7wO8HzBLRiUNQOuxEbDJIh0vMw7RqNr7w7-67pgmDI0w/viewform?entry.1624436263=${encodeURIComponent(quizURL)}" width="100%" height="650px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`
     }
 }
