4b4bc6b38a9beaefab8fc9d7e3fbefb25f03499d
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-28 20:27:26 -0700

    add custom quiz list

---
+++ js/custom-quiz-list.js
@@ -0,0 +1,14 @@
+window.onload = requestData()
+
+async function requestData() {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbw7N1XKrFFc2BkPEZCHa43luCCD0vCprS5QcRbQ1UFv5oSXHaD43GFXTYvQXGfFNxsCbA/exec?action=getlist")
+    const quizList = await response.json()
+    console.log(`Quiz list: ${quizList}`)
+
+    let listTable = document.getElementById("quiz-list-table")
+    let url
+    for (let i = 0; i < quizList.length; i++) {
+        url = quizList[i]
+        listTable.innerHTML += `<tr><td>${`<a href="${url}">Link to Quiz</a>`}</td></tr>`
+    }
+}
