3ffad0654e13eab2d521c0f37bfe1727b2659905
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-28 17:55:38 -0700

    add form

---
+++ js/submit-quiz.js
@@ -0,0 +1,13 @@
+window.onload = setup()
+
+let searchParams = new URLSearchParams(window.location.search)
+
+function setup() {
+    if (!(searchParams.has("url"))) {
+        window.location.href = "/"
+    } else {
+        let quizURL = "https://" + window.location.host + "/geography/quiz" + atob(searchParams.get("url"))
+        formDiv = document.getElementById("form-container")
+        formDiv.innerHTML = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmVw7wO8HzBLRiUNQOuxEbDJIh0vMw7RqNr7w7-67pgmDI0w/viewform?embedded=true&entry.1624436263=${encodeURIComponent(quizURL)}" width="640" height="448" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`
+    }
+}
