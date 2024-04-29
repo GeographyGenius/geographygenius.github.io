53176ff3ab19e33d33878de543ba3bda39ecf6b4
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-04-28 18:30:51 -0700

    width 100%

--- js/submit-quiz.js
+++ js/submit-quiz.js
@@ -8,7 +8,8 @@ function setup() {
         window.location.href = "/"
     } else {
         let quizURL = "https://" + window.location.host + "/geography/quiz" + atob(searchParams.get("data"))
+        console.log(quizURL)
         formDiv = document.getElementById("form-container")
-        formDiv.innerHTML = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmVw7wO8HzBLRiUNQOuxEbDJIh0vMw7RqNr7w7-67pgmDI0w/viewform?embedded=true&entry.1624436263=${encodeURIComponent(quizURL)}" width="640" height="448" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`
+        formDiv.innerHTML = `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdmVw7wO8HzBLRiUNQOuxEbDJIh0vMw7RqNr7w7-67pgmDI0w/viewform?entry.1624436263=${encodeURIComponent(quizURL)}" width="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`
     }
 }
