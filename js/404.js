35af62a421f7048fe28d10079ba76a0b3a3ad4f6
redspacecat <162653322+redspacecat@users.noreply.github.com>
2024-08-08 09:00:49 -0700

    re-add url checking

--- js/404.js
+++ js/404.js
@@ -8,18 +8,20 @@ if (window.location.pathname.includes("/q/")) {
         let redirectURL = "https://tinyurl.com/" + tinyurl_id
         location.href = redirectURL
     }
-    // try {
-    //     let response = httpGet(redirectURL)
-    //     if (response.includes("Geography Genius - Quiz") && response.includes("Loading quiz...")) {
-    //         window.location.href = redirectURL
-    //     } else {
-    //         window.location.href = "/404"
-    //     }
-    // }
-    // catch(err) {
-    //     console.log("Error - " + err)
-    //     window.location.href = "/404"
-    // }
+    //get rid of this if it doesn't work
+    try {
+        let response = httpGet(redirectURL)
+        if (response.includes("Geography Genius - Quiz") && response.includes("Loading quiz...")) {
+            window.location.href = redirectURL
+        } else {
+            window.location.href = "/404"
+        }
+    }
+    catch(err) {
+        console.log("Error - " + err)
+        window.location.href = "/404"
+    }
+
 } else {
     document.getElementById("actually-everything").hidden = false
     document.getElementsByTagName("title")[0].innerHTML = "404 - Page Not Found - Geography Genius"
